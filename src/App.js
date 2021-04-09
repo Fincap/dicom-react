897cecc98a346bd797f44c490f664d368f927895
fincap <finnkerman@gmail.com>
2021-04-10 00:34:06 +1000

    Added additional switches for other states.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./components/Header";
 import UploadFile from "./components/UploadFile";
+import LoadingScreen from "./components/LoadingScreen";
 import { Container } from "@material-ui/core";
 
 /* List of states:
@@ -14,15 +15,23 @@ DICOM_VIEW
 const App = () => {
   const [state, setState] = useState("SELECT_FILES");
 
-  const beginLoad = (fileList) => {
+  const beginLoad = async (fileList) => {
     console.log(fileList);
     setState("PYTHON_INIT");
+    await new Promise((r) => setTimeout(r, 2000)); // Sample loading time until Pyodide setup actually implemented.
+    setState("LOADING_IMAGESET");
   };
 
   const switchStates = () => {
     switch (state) {
       case "SELECT_FILES":
         return <UploadFile onUpload={beginLoad} />;
+
+      case "PYTHON_INIT":
+        return <LoadingScreen loadingText="Loading Python" />;
+
+      case "LOADING_IMAGESET":
+        return <LoadingScreen loadingText="Loading DICOM data" />;
 
       default:
         return <p>An error has occured</p>;
