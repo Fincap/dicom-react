981f40627a33ccafdafaa721928c454f70eb2200
fincap <finnkerman@gmail.com>
2021-04-10 00:22:58 +1000

    Added state switching to main App component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import UploadFile from "./components/UploadFile";
 import { Container } from "@material-ui/core";
 
+/* List of states:
+SELECT_FILES
+PYTHON_INIT
+LOADING_IMAGESET
+DICOM_VIEW
+*/
+
 const App = () => {
+  const [state, setState] = useState("SELECT_FILES");
+
   const beginLoad = (fileList) => {
     console.log(fileList);
+    setState("PYTHON_INIT");
+  };
+
+  const switchStates = () => {
+    switch (state) {
+      case "SELECT_FILES":
+        return <UploadFile onUpload={beginLoad} />;
+
+      default:
+        return <p>An error has occured</p>;
+    }
   };
 
   return (
     <Container className="App" maxWidth="md" disableGutters>
       <Header />
       <br />
-      <UploadFile onUpload={beginLoad} />
+      {switchStates()}
     </Container>
   );
 };
