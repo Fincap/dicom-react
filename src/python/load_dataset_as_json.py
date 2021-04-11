ae80c8bdec54fc74f5fe5b3797a4268908179170
fincap <finnkerman@gmail.com>
2021-04-11 19:50:30 +1000

    Load dataset as json script restructured.

--- src/python/load_dataset_as_json.py
+++ src/python/load_dataset_as_json.py
@@ -1,21 +1,23 @@
-# pylint: disable=import-error
-from js import raw_loaded   # Access the context passed into the worker
-import pydicom
-print("here")
-filenames = []
+def load_dataset_as_json():
+    # pylint: disable=import-error
+    from js import raw_loaded   # Access the context passed into the worker
+    import pydicom
+    filenames = []
 
-# pylint: disable=undefined-variable
-for i, raw_image in enumerate(raw_loaded):
-    filename = str(i) + ".dcm"
-    filenames.append(filename)
-    with open(filename, "wb") as f:
-        f.write(raw_image.tobytes())
+    # pylint: disable=undefined-variable
+    for i, raw_image in enumerate(raw_loaded):
+        filename = str(i) + ".dcm"
+        filenames.append(filename)
+        with open(filename, "wb") as f:
+            f.write(raw_image.tobytes())
 
-dataset_list_json = []
+    dataset_list_json = []
 
-for dicom_file in filenames:
-    ds = pydicom.dcmread(filename)
-    dataset_list_json.append(ds.to_json())
+    for filename in filenames:
+        ds = pydicom.dcmread(filename)
+        dataset_list_json.append(ds.to_json())
 
-dataset_list_json  # Return the result
-"done"
+    return dataset_list_json
+
+
+load_dataset_as_json()
