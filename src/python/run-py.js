8fa5c092518544d3273734249f18ed67861b5f77
fincap <finnkerman@gmail.com>
2021-04-13 01:31:32 +1000

    Exception handling for run-py script.

--- src/python/run-py.js
+++ src/python/run-py.js
@@ -4,8 +4,12 @@ const runPythonScript = (script, callback, context) => {
   fetch(script)
     .then((src) => src.text())
     .then(async (code) => {
-      const { results, error } = await asyncRun(code, context);
-      callback({ results, error });
+      try {
+        const { results, error } = await asyncRun(code, context);
+        callback({ results, error });
+      } catch (error) {
+        callback({ error });
+      }
     });
 };
 
